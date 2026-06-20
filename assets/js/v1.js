028060a21d3e4fdf3fd80f3fcc1189b7591b5394
RoGroup <63790353+RoGroup@users.noreply.github.com>
2026-06-20 17:08:18 +0100

    Update v1.js

--- assets/js/v1.js
+++ assets/js/v1.js
@@ -11,7 +11,7 @@ $('.play-button').on('click', (e) => {
 
   if (!playing) {
     $('.play-button i').removeClass('fa-play').addClass('fa-pause');
-    audio = new Audio(`https://live.freshradio.pw?cacheBuster=${Math.ceil(Math.random() * 40000)}`);
+    audio = new Audio(`https://live.upbeat.pw?cacheBuster=${Math.ceil(Math.random() * 40000)}`);
     audio.volume = volume;
     playing = true;
     audio.play();
@@ -46,8 +46,8 @@ const fetchSongData = () => {
   .then(({ song }) => {
     $('.song-name').text(song.title);
     $('.song-artist').text(song.artist);
-    $('.song-art:not(.dj-art)').attr('src', song.graphic.medium || "https://freshradio.pw/images/FRESH%20NEW.png")
-    $('.song-background img').attr('src', song.graphic.medium || "https://freshradio.pw/images/FRESH%20NEW.png")
+    $('.song-art:not(.dj-art)').attr('src', song.graphic.medium || "https://rogroup.github.io/images/FRESH%20NEW.png")
+    $('.song-background img').attr('src', song.graphic.medium || "https://rogroup.github.io/images/FRESH%20NEW.png")
   });
 };
 
@@ -59,11 +59,11 @@ const fetchSlotData = () => {
   .then((result) => {
     $('.dj-name').text(`${result.user.firstName} ${result.user.lastName}`);
     $('.dj-art').attr('src', result.user.avatar);
-    $('.dj-background img').attr('src', result.user.avatar || "https://freshradio.pw/images/FRESH%20NEW.png");
+    $('.dj-background img').attr('src', result.user.avatar || "https://rogroup.github.io/images/FRESH%20NEW.png");
 
     $('.presenter-live .presenter-name').text(`${result.user.firstName} ${result.user.lastName}`);
     $('.presenter-live .presenter-time').text(`${moment.unix(result.start).format('HH:mm')}-${moment.unix(result.end).format('HH:mm')}`);
-    $('.presenter-live .presenter-image').attr('src', result.user.avatar || "https://freshradio.pw/images/FRESH%20NEW.png");
+    $('.presenter-live .presenter-image').attr('src', result.user.avatar || "https://rogroup.github.io/images/FRESH%20NEW.png");
   })
   .catch(() => {
     $('.dj-name').text('Fresh Stream');
@@ -72,7 +72,7 @@ const fetchSlotData = () => {
 
     $('.presenter-live .presenter-name').text('Fresh Stream');
     $('.presenter-live .presenter-time').text("");
-    $('.presenter-live .presenter-image').attr('src', 'https://freshradio.pw/images/FRESH%20NEW.png');
+    $('.presenter-live .presenter-image').attr('src', 'https://rogroup.github.io/images/FRESH%20NEW.png');
   });
 
 	fetch('https://api.radiopanel.co/api/v1/slots/next', {
@@ -82,12 +82,12 @@ const fetchSlotData = () => {
   .then((result) => {
     $('.presenter-next .presenter-name').text(`${result.user.firstName} ${result.user.lastName}`);
     $('.presenter-next .presenter-time').text(`${moment.unix(result.start).format('HH:mm')}-${moment.unix(result.end).format('HH:mm')}`);
-    $('.presenter-next .presenter-image').attr('src', result.user.avatar || "https://freshradio.pw/images/FRESH%20NEW.png");
+    $('.presenter-next .presenter-image').attr('src', result.user.avatar || "https://rogroup.github.io/images/FRESH%20NEW.png");
   })
   .catch(() => {
     $('.presenter-next .presenter-name').text('Fresh Stream');
     $('.presenter-next .presenter-time').text("");
-    $('.presenter-next .presenter-image').attr('src', 'https://freshradio.pw/images/FRESH%20NEW.png');
+    $('.presenter-next .presenter-image').attr('src', 'https://rogroup.github.io/images/FRESH%20NEW.png');
   });
 
 	fetch('https://api.radiopanel.co/api/v1/slots/later', {
@@ -97,12 +97,12 @@ const fetchSlotData = () => {
   .then((result) => {
     $('.presenter-later .presenter-name').text(`${result.user.firstName} ${result.user.lastName}`);
     $('.presenter-later .presenter-time').text(`${moment.unix(result.start).format('HH:mm')}-${moment.unix(result.end).format('HH:mm')}`);
-    $('.presenter-later .presenter-image').attr('src', result.user.avatar || "https://freshradio.pw/images/FRESH%20NEW.png");
+    $('.presenter-later .presenter-image').attr('src', result.user.avatar || "https://rogroup.github.io/images/FRESH%20NEW.png");
   })
   .catch(() => {
     $('.presenter-later .presenter-name').text('Fresh Stream');
     $('.presenter-later .presenter-time').text("");
-    $('.presenter-later .presenter-image').attr('src', 'https://freshradio.pw/images/FRESH%20NEW.png');
+    $('.presenter-later .presenter-image').attr('src', 'https://rogroup.github.io/images/FRESH%20NEW.png');
   });
 
   fetch('https://app.radiopanel.co/api/v1/song-history?pagesize=3', {
@@ -114,7 +114,7 @@ const fetchSlotData = () => {
       _embedded.forEach((songPlay) => {
         $('.song-history-container').append(`
           <div class="song-history-item">
-            <img src="${(songPlay.song.graphic && songPlay.song.graphic.large) || "https://freshradio.pw/images/FRESH%20NEW.png"}" alt="" class="song-history-item-image">
+            <img src="${(songPlay.song.graphic && songPlay.song.graphic.large) || "https://rogroup.github.io/images/FRESH%20NEW.png"}" alt="" class="song-history-item-image">
             <div class="song-history-item-info">
               <p class="song-history-item-subtitle">Played ${moment(songPlay.createdAt).fromNow()}</p>
               <p class="song-history-item-name">${songPlay.song.artist} - ${songPlay.song.title}</p>
